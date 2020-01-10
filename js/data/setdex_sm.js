678a0f8b3d1beb2d46ea80cca8b1275ccd53aa09
DatSpookTho <nickguin123@gmail.com>
2020-01-09 20:50:57 -0500

    Add files via upload

--- js/data/setdex_sm.js
+++ js/data/setdex_sm.js
@@ -1,49 +1,378 @@
 var SETDEX_SM = {
-	"Crobat": {
-		},
-	"Dragalge": {
-		"CS[M] You Just Posted Cringe": {"level": 100, "evs": {"sa": 252, "sd": 4, "hp": 252}, "nature": "Modest", "ability": "Poison Point", "item": "Black Sludge", "moves": ["Draco Meteor", "Sludge Wave", "Toxic Spikes", "Focus Blast"]},
-		},
-	"Dugtrio-Alola": {
-	},
-	"Farfetch'd": {
-	},
-	"Galvantula": {
-	},
-	"Heracross": {
-		"CS[M] Physically Defensive": {"level": 100, "evs": {"sd": 4, "df": 252, "hp": 252}, "nature": "Adamant", "ability": "Poison Heal", "item": "Toxic Orb", "moves": ["U-turn", "Will-O-Wisp", "Shadow Ball", "Stealth Rock"]},
-		},
-	"Kyurem": {
-		},
-	"Ludicolo": {
-		},
-	"Magearna": {
-		},
-	"Malamar": {
-		"CS[M] Z-Happy Hour": {"level": 100, "evs": {"sd": 4, "sa": 252, "sp": 252}, "nature": "Modest", "ability": "Analytic", "item": "Normalium Z", "moves": ["Happy Hour", "Flamethrower", "Flash Cannon", "Psyshock"]},
-	},
-	"Ninetales": {
-		},
-	"Pupitar": {
-	},
-	"Purugly": {
-		"CS[M] All-out Attacker": {"level": 100, "evs": {"sd": 4, "at": 252, "sp": 252}, "nature": "Jolly", "ability": "Regenerator", "item": "Fairium Z", "moves": ["Knock Off", "Pursuit", "Play Rough", "U-turn"]},
-	},
-	"Rotom": {
-		"CS[M] Speedy Special Attacker": {"level": 100, "evs": {"sa": 252, "sd": 4, "sp": 252}, "nature": "Modest", "ability": "Speed Boost", "item": "Life Orb", "moves": ["Shadow Ball", "Work Up", "Protect", "Thunderbolt"]},
-		},
-	"Rotom-Mow": {
-		},
-	"Rotom-Wash": {
-		},
-	"Torterra": {
-	},
-	"Type: Null": {
-		},
-	"Umbreon": {
-		"CS[M] Absolutely Merciless": {"level": 100, "evs": {"sd": 252, "hp": 252, "sp": 4}, "nature": "Calm", "ability": "Merciless", "item": "Leftovers", "moves": ["Recover", "Dark Pulse", "Hex", "Toxic"]},
-	},
-	"Wailord": {
-		"CS[M] Tank": {"level": 100, "evs": {"hp": 252, "df": 252, "sa": 4}, "nature": "Bold", "ability": "Pressure", "item": "Leftovers", "moves": ["Hurricane", "Water Spout", "Roost", "Defog"]},
-	},
+    "Crobat": {
+        "Physical Wall": {
+            "level": 100,
+            "evs": {"sd": 4, "df": 252, "hp": 252},
+            "nature": "Impish",
+            "ability": "Magic Guard",
+            "item": "Leftovers",
+            "moves": ["Taunt", "Circle Throw", "Brave Bird", "Roost"]
+        },
+    },
+    "Dragalge": {
+        "Special Wall": {
+            "level": 100,
+            "evs": {"hp": 252, "df": 4, "sd": 252},
+            "nature": "Calm",
+            "ability": "Poison Point",
+            "item": "Black Sludge",
+            "moves": ["Dragon Tail", "Sludge Bomb", "Toxic Spikes", "Pain Split"]
+        },
+        "Rain Dance": {
+            "level": 100,
+            "evs": {"hp": 252, "df": 4, "sd": 252},
+            "nature": "Calm",
+            "ability": "Hydration",
+            "item": "Black Sludge",
+            "moves": ["Dragon Tail", "Sludge Bomb", "Rain Dance", "Rest"]
+        },
+        "You Just Posted Cringe": {
+            "level": 100,
+            "evs": {"sa": 252, "sd": 4, "hp": 252},
+            "nature": "Modest",
+            "ability": "Poison Point",
+            "item": "Black Sludge",
+            "moves": ["Draco Meteor", "Sludge Wave", "Toxic Spikes", "Focus Blast"]
+        },
+    },
+    "Dugtrio-Alola": {
+        "Hazard Lead": {
+            "level": 100,
+            "evs": {"at": 252, "df": 4, "sp": 252},
+            "nature": "Jolly",
+            "ability": "Intimidate",
+            "item": "Focus Sash",
+            "moves": ["Steath Rock", "Spikes", "Earthquake", "Taunt"]
+        },
+        "Bulky": {
+            "level": 100,
+            "evs": {"hp": 252, "df": 56, "sp": 200},
+            "nature": "Jolly",
+            "ability": "Intimidate",
+            "item": "Leftovers",
+            "moves": ["Earthquake", "Spikes", "Taunt", "Toxic"]
+        },
+    },
+    "Farfetch'd": {
+        "Scarf": {
+            "level": 100,
+            "evs": {"at": 252, "df": 4, "sp": 252},
+            "nature": "Jolly",
+            "ability": "Defiant",
+            "item": "Choice Scarf",
+            "moves": ["Brave Bird", "Sacred Sword", "Knock Off", "U-turn"]
+        },
+        "Stick": {
+            "level": 100,
+            "evs": {"at": 252, "df": 4, "sp": 252},
+            "nature": "Jolly",
+            "ability": "Sniper",
+            "item": "Stick",
+            "moves": ["Brave Bird", "Cross Chop", "Night Slash", "First Impression"]
+        },
+    },
+    "Galvantula": {
+        "Bulky": {
+            "level": 100,
+            "evs": {"hp": 252, "df": 56, "sp": 200},
+            "nature": "Timid",
+            "ability": "Fluffy",
+            "item": "Leftovers",
+            "moves": ["Bug Buzz", "Volt Switch", "Slack Off", "Toxic"]
+        },
+        "Offensive Pivot": {
+            "level": 100,
+            "evs": {"sa": 252, "df": 4, "sp": 252},
+            "nature": "Timid",
+            "ability": "Fluffy",
+            "item": "Electrium Z",
+            "moves": ["Bug Buzz", "Volt Switch", "Slack Off", "Giga Drain"]
+        },
+    },
+    "Heracross": {
+        "Stealth Rock": {
+            "level": 100,
+            "evs": {"sd": 4, "df": 252, "hp": 252},
+            "nature": "Impish",
+            "ability": "Poison Heal",
+            "item": "Toxic Orb",
+            "moves": ["U-turn", "Will-O-Wisp", "Shadow Claw", "Stealth Rock"]
+        },
+        "Mixed Wall": {
+            "level": 100,
+            "evs": {"sd": 4, "df": 252, "hp": 252},
+            "nature": "Careful",
+            "ability": "Poison Heal",
+            "item": "Toxic Orb",
+            "moves": ["Knock Off", "Will-O-Wisp", "Seismic Toss", "Protect"]
+        },
+    },
+    "Kyurem": {
+        "Pressure Stall": {
+            "level": 100,
+            "evs": {"hp": 252, "sd": 116, "sp": 140},
+            "nature": "Calm",
+            "ability": "Pressure",
+            "item": "Leftovers",
+            "moves": ["Freeze-Dry", "Noble Roar", "Substitute", "Protect"]
+        },
+        "Choice Specs": {
+            "level": 100,
+            "evs": {"sa": 252, "df": 4, "sp": 252},
+            "nature": "Naive",
+            "ability": "Pressure",
+            "item": "Choice Specs",
+            "moves": ["Freeze-Dry", "Ice Beam", "Hidden Power [Fire]", "Stone Edge"]
+        },
+    },
+    "Ludicolo": {
+        "Rain Dance": {
+            "level": 100,
+            "evs": {"hp": 252, "df": 4, "sp": 252},
+            "nature": "Modest",
+            "ability": "Swift Swim",
+            "item": "Life Orb",
+            "moves": ["Surf", "Ice Beam", "Rain Dance", "Hidden Power [Electric]"]
+        },
+    },
+    "Magearna": {
+        "Triage Specs": {
+            "level": 100,
+            "evs": {"sa": 252, "df": 4, "sp": 252},
+            "nature": "Modest",
+            "ability": "Triage",
+            "item": "Choice Specs",
+            "moves": ["Draining Kiss", "Moonblast", "Flash Cannon", "Volt Switch"]
+        },
+        "Mixed Attacker": {
+            "level": 100,
+            "evs": {"at": 252, "df": 4, "sp": 252},
+            "nature": "Naughty",
+            "ability": "Huge Power",
+            "item": "Leftovers",
+            "moves": ["Iron Head", "Brick Break", "Moonblast", "Volt Switch"]
+        },
+    },
+    "Malamar": {
+        "Z-Happy Hour": {
+            "level": 100,
+            "evs": {"sd": 4, "sa": 252, "sp": 252},
+            "nature": "Timid",
+            "ability": "Analytic",
+            "item": "Normalium Z",
+            "moves": ["Happy Hour", "Flamethrower", "Flash Cannon", "Psyshock"]
+        },
+        "Choice Specs": {
+            "level": 100,
+            "evs": {"sd": 4, "sa": 252, "sp": 252},
+            "nature": "Modest",
+            "ability": "Analytic",
+            "item": "Choice Specs",
+            "moves": ["Flash Cannon", "Psyshock", "Thunderbolt", "Flamethrower"]
+        },
+        "Sub CM": {
+            "level": 100,
+            "evs": {"hp": 20, "sa": 252, "sp": 232},
+            "nature": "Modest",
+            "ability": "Analytic",
+            "item": "Leftovers",
+            "moves": ["Substitute", "Calm Mind", "Flash Cannon", "Flamethrower"]
+        },
+    },
+    "Ninetales": {
+        "Offensive Pivot": {
+            "level": 100,
+            "evs": {"sa": 252, "sd": 4, "sp": 252},
+            "nature": "Timid",
+            "ability": "Flash Fire",
+            "item": "Fairium Z",
+            "moves": ["Fire Blast", "Moonblast", "Extrasensory", "Nasty Plot"]
+        },
+        "Choice Scarf": {
+            "level": 100,
+            "evs": {"sa": 252, "sd": 4, "sp": 252},
+            "nature": "Timid",
+            "ability": "Flash Fire",
+            "item": "Choice Scarf",
+            "moves": ["Fire Blast", "Moonblast", "Power Gem", "Toxic"]
+        },
+        "Sun Choice Band": {
+            "level": 100,
+            "evs": {"at": 252, "sd": 4, "sp": 252},
+            "nature": "Jolly",
+            "ability": "Flower Gift",
+            "item": "Choice Band",
+            "moves": ["Flare Blitz", "Play Rough", "Extremespeed", "Solar Beam"]
+        },
+    },
+    "Pupitar": {
+        "Dragon Dance": {
+            "level": 100,
+            "evs": {"at": 252, "sd": 4, "sp": 252},
+            "nature": "Jolly",
+            "ability": "Shed Skin",
+            "item": "Eviolite",
+            "moves": ["Stone Edge", "Earthquake", "Dragon Dance", "Rest"]
+        },
+        "Stealth Rock Tank": {
+            "level": 100,
+            "evs": {"at": 252, "hp": 128, "df": 128},
+            "nature": "Jolly",
+            "ability": "Shed Skin",
+            "item": "Eviolite",
+            "moves": ["Stone Edge", "Earthquake", "Dragon Dance", "Rest"]
+        },
+    },
+    "Purugly": {
+        "Offensive Pivot": {
+            "level": 100,
+            "evs": {"sd": 4, "at": 252, "sp": 252},
+            "nature": "Jolly",
+            "ability": "Regenerator",
+            "item": "Life Orb",
+            "moves": ["Knock Off", "Pursuit", "Play Rough", "U-turn"]
+        },
+    },
+    "Rotom": {
+        "Speed Boost": {
+            "level": 100,
+            "evs": {"sa": 252, "sd": 4, "sp": 252},
+            "nature": "Modest",
+            "ability": "Speed Boost",
+            "item": "Life Orb",
+            "moves": ["Shadow Ball", "Work Up", "Protect", "Thunderbolt"]
+        },
+        "Defog Pivot": {
+            "level": 100,
+            "evs": {"sa": 252, "hp": 160, "sp": 96},
+            "nature": "Modest",
+            "ability": "Levitate",
+            "item": "Eviolite",
+            "moves": ["Shadow Ball", "Volt Switch", "Defog", "Recover"]
+        },
+    },
+    "Rotom-Mow": {
+        "Shift Gear": {
+            "level": 100,
+            "evs": {"at": 252, "hp": 88, "sp": 168},
+            "nature": "Adamant",
+            "ability": "Steelworker",
+            "item": "Life Orb",
+            "moves": ["Psychic Fangs", "Leaf Blade", "Iron Head", "Shift Gear"]
+        },
+    },
+    "Rotom-Wash": {
+        "Physical Wall": {
+            "level": 100,
+            "evs": {"hp": 252, "df": 252, "sa": 4},
+            "nature": "Bold",
+            "ability": "Cursed Body",
+            "item": "Leftovers",
+            "moves": ["Surf", "Shadow Ball", "Will-O-Wisp", "Pain Split"]
+        },
+        "Stall Breaker": {
+            "level": 100,
+            "evs": {"sa": 252, "sp": 252, "sd": 4},
+            "nature": "Timid",
+            "ability": "Water Absorb",
+            "item": "Waterium Z",
+            "moves": ["Hydro Pump", "Shadow Ball", "Taunt", "Pain Split"]
+        },
+        "Trick Scarf": {
+            "level": 100,
+            "evs": {"sa": 252, "sp": 252, "sd": 4},
+            "nature": "Timid",
+            "ability": "Water Absorb",
+            "item": "Choice Scarf",
+            "moves": ["Surf", "Shadow Ball", "Trick", "Defog"]
+        },
+    },
+    "Torterra": {
+        "Physical Wall": {
+            "level": 100,
+            "evs": {"hp": 252, "df": 252, "sd": 4},
+            "nature": "Impish",
+            "ability": "Rock Head",
+            "item": "Leftovers",
+            "moves": ["Wood Hammer", "Earthquake", "Synthesis", "Stealth Rock"]
+        },
+        "Drought": {
+            "level": 100,
+            "evs": {"hp": 252, "df": 252, "sd": 4},
+            "nature": "Relaxed",
+            "ability": "Drought",
+            "item": "Leftovers",
+            "moves": ["Solar Beam", "Earthquake", "Synthesis", "Stealth Rock"]
+        },
+        "Choice Band": {
+            "level": 100,
+            "evs": {"hp": 168, "at": 252, "sp": 88},
+            "nature": "Adamant",
+            "ability": "Rock Head",
+            "item": "Leftovers",
+            "moves": ["Wood Hammer", "Earthquake", "Head Smash", "Outrage"]
+        },
+    },
+    "Type: Null": {
+        "Choice Scarf": {
+            "level": 100,
+            "evs": {"at": 252, "sp": 252, "df": 4},
+            "nature": "Adamant",
+            "ability": "Tinted Lens",
+            "item": "Choice Scarf",
+            "moves": ["Double Edge", "Iron Head", "U-turn", "Facade"]
+        },
+        "Swords Dance" {
+            "level": 100,
+            "evs": {"at": 252, "sp": 252, "df": 4},
+            "nature": "Adamant",
+            "ability": "Tinted Lens",
+            "item": "Choice Scarf",
+            "moves": ["Double Edge", "Iron Head", "Bullet Punch", "Swords Dance"]
+        },
+    },
+    "Umbreon": {
+        "Merciless Wall": {
+            "level": 100,
+            "evs": {"sd": 252, "hp": 252, "sp": 4},
+            "nature": "Calm",
+            "ability": "Merciless",
+            "item": "Leftovers",
+            "moves": ["Foul Play", "Hex", "Toxic", "Recover"]
+        },
+        "Merciless Attacker": {
+            "level": 100,
+            "evs": {"sa": 252, "hp": 36, "sp": 220},
+            "nature": "Modest",
+            "ability": "Merciless",
+            "item": "Leftovers",
+            "moves": ["Hex", "Hidden Power [Ground]", "Recover", "Toxic Thread"]
+        },
+    },
+    "Wailord": {
+        "SpD Defog": {
+            "level": 100,
+            "evs": {"hp": 252, "sd": 252, "sa": 4},
+            "nature": "Calm",
+            "ability": "Oblivious",
+            "item": "Leftovers",
+            "moves": ["Hurricane", "Scald", "Roost", "Defog"]
+        },
+        "Def Defog": {
+            "level": 100,
+            "evs": {"hp": 252, "df": 252, "sa": 4},
+            "nature": "Bold",
+            "ability": "Oblivious",
+            "item": "Leftovers",
+            "moves": ["Hurricane", "Scald", "Roost", "Defog"]
+        },
+        "Attacker": {
+            "level": 100,
+            "evs": {"sa": 252, "sp": 252, "sd": 4},
+            "nature": "Modest",
+            "ability": "Oblivious",
+            "item": "Flyinium Z",
+            "moves": ["Hurricane", "Hydro Pump", "Roost", "Defog"]
+        },
+    },
 };
